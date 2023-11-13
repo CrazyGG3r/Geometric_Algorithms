20599dbfae8b7b49db57829a7e6b2f05985a6425
Shaheer Ul Islam <shaheerislam.islam179@gmail.com>
2023-11-13 09:46:45 +0500

    line anim

--- CLASSES.py
+++ CLASSES.py
@@ -4,10 +4,10 @@ import random as r
 
 class point:
     def __init__(self,x,y,color):
-        self.x = x;
-        self.y = y;
-        self.color = color;
-        self.radius = 5;
+        self.x = x
+        self.y = y
+        self.color = color
+        self.radius = 5
 
 
     def draw(self, surface):
@@ -15,4 +15,24 @@ class point:
 
     def drawtopoint(self,screen,outr):
         pygame.draw.line(screen,self.color,(self.x,self.y),(outr.x,outr.y))
+
+class line:
+    def __init__(self,w,duration,color):
+        self.w = w
+        self.duration = duration
+        self.elapsed = 0
+        self.color = color;
+
+    def update(self,dt):
+        self.elapsed +=dt
+        if self.elapsed >= self.duration:
+            self.elapsed = self.duration
+
+    def draw(self,screen,point1,point2):
+         progress = self.elapsed / self.duration
+         current_x = int(point1.x + (point2.x - point1.x) * progress)
+         current_y = int(point1.y + (point2.y - point1.y) * progress)
+         pygame.draw.line(screen, self.color, (point1.x,point1.y),(current_x, current_y), self.w)
+
+
 
--- MAIN.py
+++ MAIN.py
@@ -9,49 +9,27 @@ pygame.init()
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Line Segment Intersection Demo")
+
 point = c.point(100,300,(233,2,33))
 p2 = c.point(600,400,(33,55,66))
-# Define line segment endpoints
-line1 = [(100, 100), (400, 400)]
-line2 = [(200, 300), (600, 100)]
+l = c.line(1,10,(220,220,100))
 
-# Function to check if two line segments intersect using the cross product rule
-def do_lines_intersect(line1, line2):
-    x1, y1 = line1[0]
-    x2, y2 = line1[1]
-    x3, y3 = line2[0]
-    x4, y4 = line2[1]
-
-    def orientation(p, q, r):
-        val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
-        if val == 0:
-            return 0
-        return 1 if val > 0 else 2
-
-    o1 = orientation((x1, y1), (x2, y2), (x3, y3))
-    o2 = orientation((x1, y1), (x2, y2), (x4, y4))
-    o3 = orientation((x3, y3), (x4, y4), (x1, y1))
-    o4 = orientation((x3, y3), (x4, y4), (x2, y2))
-
-    if o1 != o2 and o3 != o4:
-        return True
-
-    return False
-
+clock = pygame.time.Clock()
 # Main game loop
 while True:
+    dt = clock.tick(60) / 1000.0
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    # Check for intersection
-    intersection = do_lines_intersect(line1, line2)
+
 
     # Draw line segments
-    point.drawtopoint(screen,p2)
+    
+    l.update(dt)
+    l.draw(screen,point,p2)
     point.draw(screen)
     p2.draw(screen)
-    
     # Update the display
     pygame.display.flip()
