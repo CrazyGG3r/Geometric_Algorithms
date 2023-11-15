f2c270485900362ce111a328a46666ce5e0a0e53
Saeed Saleem <Saeed Saleem@DESKTOP-MCFD076>
2023-11-16 04:23:57 +0500

    graham scan hogya, quick elimination galat kaam kr rha ha aur brute nhi hora

--- CONVEXHULL.py
+++ CONVEXHULL.py
@@ -1,3 +1,6 @@
+from functools import cmp_to_key
+from itertools import combinations
+import CLASSES as c
 class JarvisMarch:
     def __init__(self, points):
         self.points = points
@@ -44,6 +47,125 @@ class JarvisMarch:
 
         result = [(self.points[i]) for i in hull]
         return result
+
+
+
+
+
+
+
+
+class GrahamScan:
+    def __init__(self, points):
+        self.points = points
+        self.convex_hull = []
+
+    def orientation(self, p, q, r):
+        val = ((q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y))
+        if val == 0:
+            return 0  # collinear
+        elif val > 0:
+            return 1  # clockwise
+        else:
+            return 2  # counterclockwise
+
+    def compare(self, p1, p2):
+        o = self.orientation(self.p0, p1, p2)
+        if o == 0:
+            if self.distSq(self.p0, p2) >= self.distSq(self.p0, p1):
+                return -1
+            else:
+                return 1
+        else:
+            if o == 2:
+                return -1
+            else:
+                return 1
+
+    def distSq(self, p1, p2):
+        return ((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y))
+
+    def next_to_top(self, S):
+        return S[-2]
+
+    def convex_hull_graham_scan(self):
+        n = len(self.points)
+
+        if n < 3:
+            return
+
+        ymin = self.points[0].y
+        min_index = 0
+
+        for i in range(1, n):
+            y = self.points[i].y
+
+            if y < ymin or (ymin == y and self.points[i].x < self.points[min_index].x):
+                ymin = self.points[i].y
+                min_index = i
+
+        self.points[0], self.points[min_index] = self.points[min_index], self.points[0]
+        self.p0 = self.points[0]
+
+        self.points = sorted(self.points, key=cmp_to_key(self.compare))
+
+        m = 1
+        for i in range(1, n):
+            while i < n - 1 and self.orientation(self.p0, self.points[i], self.points[i + 1]) == 0:
+                i += 1
+
+            self.points[m] = self.points[i]
+            m += 1
+
+        if m < 3:
+            return
+
+        S = []
+        S.append(self.points[0])
+        S.append(self.points[1])
+        S.append(self.points[2])
+
+        for i in range(3, m):
+            while len(S) > 1 and self.orientation(self.next_to_top(S), S[-1], self.points[i]) != 2:
+                S.pop()
+            S.append(self.points[i])
+
+        return S
+
+class QuickElimination:
+    def __init__(self, points):
+        self.points = points
+
+    def sort_points(self):
+        self.points.sort(key=lambda p: p.x)
+
+    def is_right(self, p1, p2, p3):
+        det = (p3.y - p1.y) * (p2.x - p1.x) - (p2.y - p1.y) * (p3.x - p1.x)
+        return det < 0
+
+    def quick_elimination(self):
+        hull = []
+        self.sort_points()
+
+        # Add the first two points to the hull
+        hull.append(self.points[0])
+        hull.append(self.points[1])
+
+        for i in range(2, len(self.points)):
+            while len(hull) >= 2 and not self.is_right(hull[-2], hull[-1], self.points[i]):
+                hull.pop()
+
+            hull.append(self.points[i])
+
+        return hull
+
+
+
+
+
+
+
+
 
 
 
--- MAIN.py
+++ MAIN.py
@@ -4,66 +4,156 @@ import sys
 import CLASSES as c
 import random as r
 import CONVEXHULL as cv 
+from functools import cmp_to_key
 pygame.init()
 
-# Set up the window
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Line Segment Intersection Demo")
 
+#SETUP AND POINT GENERATE
+width, height = 800, 600
 point = c.point(100,300,(233,2,33))
 p2 = c.point(600,400,(33,55,66))
 ldur = 1
 l = c.line(2,ldur,(200 ,200,30))
 ps = []
 for _ in range(10):
-        x = r.randint(0,width-10)
-        y = r.randint(0,height-10)
-        ps.append(c.point(x, y, (r.randint(0,255),r.randint(0,255),r.randint(0,255))))
+    x = r.randint(0,width-10)
+    y = r.randint(0,height-10)
+    ps.append(c.point(x, y, (r.randint(0,255),r.randint(0,255),r.randint(0,255))))
 
+#TAKING INPUT
+choice = int(input("Press 1 for Jarvis March\nPress 2 for Graham Scan\nPress 3 for Quick Elimination\n"))
 
-for a in range(0,len(ps)):
-    print("(",ps[a].x,",",ps[a].y,")")
-jarvis_march = cv.JarvisMarch(ps)
-cvh = jarvis_march.convex_hull()
-print(type(cvh[0]))
-
-clock = pygame.time.Clock()
-# Main game loop
-i = 0
-curr = cvh[i+1]
-prev = cvh[i]
-space = False
-while True:
-     
-    dt = clock.tick(60) / 1000.0
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            space = True
-    if space == False:
-        continue
+#CONVEX HULLS
+if choice == 1:
+    pygame.display.set_caption("Line Segment Intersection Demo")
+    screen = pygame.display.set_mode((width, height))
+    jarvis_march = cv.JarvisMarch(ps)
+    cvh = jarvis_march.convex_hull()
+    clock = pygame.time.Clock()
+    # Main game loop
+    i = 0
+    curr = cvh[i+1]
+    prev = cvh[i]
+    space = False
+    while True:
+        dt = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                space = True
+        if space == False:
+            continue
     
-    for a in ps:
-        a.draw(screen)
+        for a in ps:
+            a.draw(screen)
         
-    l.update(dt)
-    if l.elapsed != ldur:
-        l.draw(screen,prev,curr)
-    else:
-        if i == len(cvh)-1:
-            curr = cvh[0]
-            prev = cvh[-1]
+        l.update(dt)
+        if l.elapsed != ldur:
+            l.draw(screen,prev,curr)
         else:
-            prev = curr
-            curr = cvh[i+1]
-            i = i+1
-        l.elapsed = 0
-        l.draw(screen,prev,curr)
-    for a in ps:
-        a.draw(screen)
-    # Update the display
-    pygame.display.flip()
+            if i == len(cvh)-1:
+                curr = cvh[0]
+                prev = cvh[-1]
+            else:
+                prev = curr
+                curr = cvh[i+1]
+                i = i+1
+            l.elapsed = 0
+            l.draw(screen,prev,curr)
+        for a in ps:
+            a.draw(screen)
+        # Update the display
+        pygame.display.flip()
+
+elif choice == 2:
+    pygame.display.set_caption("Line Segment Intersection Demo")
+    screen = pygame.display.set_mode((width, height))
+    graham_scan = cv.GrahamScan(ps)
+    cvh = graham_scan.convex_hull_graham_scan()
+    clock = pygame.time.Clock()
+    # Main game loop
+    i = 0
+    curr = cvh[i+1]
+    prev = cvh[i]
+    space = False
+    while True:
+        dt = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                space = True
+        if space == False:
+            continue
+    
+        for a in ps:
+            a.draw(screen)
+        
+        l.update(dt)
+        if l.elapsed != ldur:
+            l.draw(screen,prev,curr)
+        else:
+            if i == len(cvh)-1:
+                curr = cvh[0]
+                prev = cvh[-1]
+            else:
+                prev = curr
+                curr = cvh[i+1]
+                i = i+1
+            l.elapsed = 0
+            l.draw(screen,prev,curr)
+        for a in ps:
+            a.draw(screen)
+        # Update the display
+        pygame.display.flip()
+
+elif choice == 3:
+    pygame.display.set_caption("Line Segment Intersection Demo")
+    screen = pygame.display.set_mode((width, height))
+    qe = cv.QuickElimination(ps)
+    cvh = qe.quick_elimination()
+    clock = pygame.time.Clock()
+    # Main game loop
+    i = 0
+    curr = cvh[i+1]
+    prev = cvh[i]
+    space = False
+    while True:
+        dt = clock.tick(60) / 1000.0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                space = True
+        if space == False:
+            continue
+    
+        for a in ps:
+            a.draw(screen)
+        
+        l.update(dt)
+        if l.elapsed != ldur:
+            l.draw(screen,prev,curr)
+        else:
+            if i == len(cvh)-1:
+                curr = cvh[0]
+                prev = cvh[-1]
+            else:
+                prev = curr
+                curr = cvh[i+1]
+                i = i+1
+            l.elapsed = 0
+            l.draw(screen,prev,curr)
+        for a in ps:
+            a.draw(screen)
+        # Update the display
+        pygame.display.flip()
+
+    
+else:
+    print("INVALID")
 
